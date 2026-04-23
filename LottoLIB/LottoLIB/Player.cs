1a1b01dc1225bde637489182fff6b1108628dc90
Niki <zxcvbrtm12345@gmail.com>
2026-04-23 22:51:37 +0300

    В файл Player.cs добавлен класс Player с состоянием игрока и подсчётом закрытых ячеек

--- LottoLIB/LottoLIB/Player.cs
+++ LottoLIB/LottoLIB/Player.cs
@@ -1,10 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace LottoLIB
+﻿namespace LottoLIB
 {
-    internal class Player
+    public class Player
     {
+        public string Name { get; }
+        public LottoCard Card { get; private set; }
+        public bool HasWon { get; private set; }
+        public int CoveredCount { get; private set; }
+
+        public Player(string name)
+        {
+            Name = name;
+            Card = new LottoCard();
+        }
+
+        public void MarkNumber(int number)
+        {
+            if (!Card.ContainsNumber(number)) return;
+            Card.MarkNumber(number);
+            UpdateCoveredCount();
+        }
+
+        private void UpdateCoveredCount()
+        {
+            CoveredCount = 0;
+            for (int row = 0; row < 3; row++)
+                for (int col = 0; col < 9; col++)
+                    if (Card.Numbers[row, col] != 0 && Card.Marked[row, col]) CoveredCount++;
+        }
+
+        public bool CheckWin()
+        {
+            HasWon = Card.IsFullyCovered();
+            return HasWon;
+        }
+
+        public void Reset()
+        {
+            Card = new LottoCard();
+            HasWon = false;
+            CoveredCount = 0;
+        }
     }
 }
